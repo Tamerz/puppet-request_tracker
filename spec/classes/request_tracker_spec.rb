a8a130da5b766cd88be9dc174eb68be7c3cf30d8
Tames McTigue <tames.mctigue@northwestern.edu>
2018-02-18 10:22:28 +0300

    Updated style of tests to match the Ruby style guide

--- spec/classes/request_tracker_spec.rb
+++ spec/classes/request_tracker_spec.rb
@@ -4,11 +4,8 @@ describe 'request_tracker' do
   on_supported_os(facterversion: '2.4').each do |os, os_facts|
     context "on #{os}" do
       let(:facts) { os_facts }
-
       it { is_expected.to compile }
-
       it { is_expected.to contain_package('gcc') }
-
     end
   end
 end
